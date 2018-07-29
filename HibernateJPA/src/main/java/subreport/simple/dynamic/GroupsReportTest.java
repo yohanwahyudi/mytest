8b755351c68fe5e8776bbd39ea1be8002bc319f8
Yohan Wahyudi <wahyudi.yohan1@gmail.com>
2018-07-29 16:29:41 +0700

    dynamic

---
+++ HibernateJPA/src/main/java/subreport/simple/dynamic/GroupsReportTest.java
@@ -0,0 +1,177 @@
+package subreport.simple.dynamic;
+
+import java.awt.Color;
+
+import net.sf.jasperreports.view.JasperDesignViewer;
+import net.sf.jasperreports.view.JasperViewer;
+import subreport.simple.dependency.BaseDjReportTest;
+import ar.com.fdvs.dj.domain.AutoText;
+import ar.com.fdvs.dj.domain.DJCalculation;
+import ar.com.fdvs.dj.domain.DynamicReport;
+import ar.com.fdvs.dj.domain.Style;
+import ar.com.fdvs.dj.domain.builders.ColumnBuilder;
+import ar.com.fdvs.dj.domain.builders.DynamicReportBuilder;
+import ar.com.fdvs.dj.domain.builders.GroupBuilder;
+import ar.com.fdvs.dj.domain.constants.Border;
+import ar.com.fdvs.dj.domain.constants.Font;
+import ar.com.fdvs.dj.domain.constants.GroupLayout;
+import ar.com.fdvs.dj.domain.constants.HorizontalAlign;
+import ar.com.fdvs.dj.domain.constants.Transparency;
+import ar.com.fdvs.dj.domain.constants.VerticalAlign;
+import ar.com.fdvs.dj.domain.entities.DJGroup;
+import ar.com.fdvs.dj.domain.entities.columns.AbstractColumn;
+import ar.com.fdvs.dj.domain.entities.columns.PropertyColumn;
+import net.sf.jasperreports.fonts.dejavu.*;
+
+public class GroupsReportTest extends BaseDjReportTest {
+
+	public DynamicReport buildReport() throws Exception {
+
+		Font font = new Font();
+		font.setFontName("DejaVu Sans");
+		font.setFontSize(18);
+		font.setBold(true);
+		font.setPdfFontName("DejaVu Sans");
+		font.setPdfFontEncoding(Font.PDF_ENCODING_Identity_H_Unicode_with_horizontal_writing);
+		font.setPdfFontEmbedded(true);
+		
+		Style detailStyle = new Style("detail");
+
+		Style headerStyle = new Style("header");
+		headerStyle.setFont(Font.ARIAL_MEDIUM_BOLD);
+		headerStyle.setBorderBottom(Border.PEN_1_POINT());
+		headerStyle.setBackgroundColor(Color.gray);
+		headerStyle.setTextColor(Color.white);
+		headerStyle.setHorizontalAlign(HorizontalAlign.CENTER);
+		headerStyle.setVerticalAlign(VerticalAlign.MIDDLE);
+		headerStyle.setTransparency(Transparency.OPAQUE);
+
+		Style headerVariables = new Style("headerVariables");
+		headerVariables.setFont(Font.ARIAL_BIG_BOLD);
+		headerVariables.setBorderBottom(Border.THIN());
+		headerVariables.setHorizontalAlign(HorizontalAlign.RIGHT);
+		headerVariables.setVerticalAlign(VerticalAlign.TOP);
+		headerVariables.setStretchWithOverflow(true);
+
+		Style groupVariables = new Style("groupVariables");
+		groupVariables.setFont(Font.ARIAL_MEDIUM_BOLD);
+		groupVariables.setTextColor(Color.BLUE);
+		groupVariables.setBorderBottom(Border.THIN());
+		groupVariables.setHorizontalAlign(HorizontalAlign.RIGHT);
+		groupVariables.setVerticalAlign(VerticalAlign.BOTTOM);
+
+		Style titleStyle = new Style("titleStyle");
+		titleStyle.setFont(font);
+		Style importeStyle = new Style();
+		importeStyle.setHorizontalAlign(HorizontalAlign.RIGHT);
+		Style oddRowStyle = new Style();
+		oddRowStyle.setBorder(Border.NO_BORDER());
+		oddRowStyle.setBackgroundColor(Color.LIGHT_GRAY);
+		oddRowStyle.setTransparency(Transparency.OPAQUE);
+
+		DynamicReportBuilder drb = new DynamicReportBuilder();
+		Integer margin = 20;
+		drb
+			.setTitleStyle(titleStyle)
+			.setTitle("November " + "2018" +" sales report")					//defines the title of the report
+			.setSubtitle("The items in this report correspond "
+					+"to the main products: DVDs, Books, Foods and Magazines")
+			.setDetailHeight(15).setLeftMargin(margin)
+			.setRightMargin(margin).setTopMargin(margin).setBottomMargin(margin)
+			.setPrintBackgroundOnOddRows(true)
+			.setGrandTotalLegend("Grand Total")
+			.setGrandTotalLegendStyle(headerVariables)
+			.setOddRowBackgroundStyle(oddRowStyle);
+
+
+		AbstractColumn columnState = ColumnBuilder.getNew()
+				.setColumnProperty("state", String.class.getName()).setTitle(
+						"State").setWidth(new Integer(85))
+				.setStyle(titleStyle).setHeaderStyle(titleStyle).build();
+
+		AbstractColumn columnBranch = ColumnBuilder.getNew()
+				.setColumnProperty("branch", String.class.getName()).setTitle(
+						"Branch").setWidth(new Integer(85)).setStyle(
+						detailStyle).setHeaderStyle(headerStyle).build();
+
+		AbstractColumn columnaProductLine = ColumnBuilder.getNew()
+				.setColumnProperty("productLine", String.class.getName())
+				.setTitle("Product Line").setWidth(new Integer(85)).setStyle(
+						detailStyle).setHeaderStyle(headerStyle).build();
+
+		AbstractColumn columnaItem = ColumnBuilder.getNew()
+				.setColumnProperty("item", String.class.getName()).setTitle(
+						"Item").setWidth(new Integer(85)).setStyle(detailStyle)
+				.setHeaderStyle(headerStyle).build();
+
+		AbstractColumn columnCode = ColumnBuilder.getNew()
+				.setColumnProperty("id", Long.class.getName()).setTitle("ID")
+				.setWidth(new Integer(40)).setStyle(importeStyle)
+				.setHeaderStyle(headerStyle).build();
+
+		AbstractColumn columnaQuantity = ColumnBuilder.getNew()
+				.setColumnProperty("quantity", Long.class.getName()).setTitle(
+						"Quantity").setWidth(new Integer(25)).setStyle(
+						importeStyle).setHeaderStyle(headerStyle).build();
+
+		AbstractColumn columnAmount = ColumnBuilder.getNew()
+				.setColumnProperty("amount", Float.class.getName()).setTitle(
+						"Amount").setWidth(new Integer(100))
+				.setPattern("$ 0.00").setStyle(importeStyle).setHeaderStyle(
+						headerStyle).build();
+
+		drb.addGlobalHeaderVariable(columnAmount, DJCalculation.SUM,headerVariables);
+		drb.addGlobalHeaderVariable(columnaQuantity, DJCalculation.SUM,headerVariables);
+		drb.addGlobalFooterVariable(columnAmount, DJCalculation.SUM,headerVariables);
+		drb.addGlobalFooterVariable(columnaQuantity, DJCalculation.SUM,headerVariables);
+		drb.setGlobalHeaderVariableHeight(25);
+		drb.setGlobalFooterVariableHeight(25);
+
+		GroupBuilder gb1 = new GroupBuilder();
+
+//		 define the criteria column to group by (columnState)
+		DJGroup g1 = gb1.setCriteriaColumn((PropertyColumn) columnState)
+//				.addFooterVariable(columnaQuantity,DJCalculation.SUM,groupVariables) // idem for the columnaQuantity column
+//				.addFooterVariable(columnAmount,DJCalculation.SUM,groupVariables) // tell the group place a variable footer of the column "columnAmount" with the SUM of allvalues of the columnAmount in this group.
+//				.addHeaderVariable(columnaQuantity,DJCalculation.SUM,groupVariables) // idem for the columnaQuantity column
+//				.addHeaderVariable(columnAmount,DJCalculation.SUM,groupVariables) // tell the group place a variable footer of the column "columnAmount" with the SUM of allvalues of the columnAmount in this group.
+//				.setGroupLayout(GroupLayout.VALUE_IN_HEADER) // tells the group how to be shown, there are manyposibilities, see the GroupLayout for more.
+//				.setFooterVariablesHeight(20)
+//				.setFooterHeight(50,true)
+//				.setHeaderVariablesHeight(35)
+				.build();
+
+		GroupBuilder gb2 = new GroupBuilder(); // Create another group (using another column as criteria)
+		DJGroup g2 = gb2.setCriteriaColumn((PropertyColumn) columnBranch) // and we add the same operations for the columnAmount and
+				.addFooterVariable(columnAmount,
+						DJCalculation.SUM) // columnaQuantity columns
+				.addFooterVariable(columnaQuantity,	DJCalculation.SUM)
+				.build();
+
+		drb.addColumn(columnState);
+		drb.addColumn(columnBranch);
+		drb.addColumn(columnaProductLine);
+		drb.addColumn(columnaItem);
+		drb.addColumn(columnCode);
+		drb.addColumn(columnaQuantity);
+		drb.addColumn(columnAmount);
+
+		drb.addGroup(g1); // add group g1
+		drb.addGroup(g2); // add group g2
+
+		drb.setUseFullPageWidth(true);
+		drb.addAutoText(AutoText.AUTOTEXT_PAGE_X_SLASH_Y, AutoText.POSITION_FOOTER, AutoText.ALIGNMENT_RIGHT);
+
+		DynamicReport dr = drb.build();
+		return dr;
+	}
+
+	public static void main(String[] args) throws Exception {
+		GroupsReportTest test = new GroupsReportTest();
+		test.testReport();
+		test.exportToJRXML();
+		JasperViewer.viewReport(test.jp);
+//		JasperDesignViewer.viewReportDesign(test.jr);
+	}
+
+}
---
+++ HibernateJPA/src/main/java/subreport/simple/fastreport/Subreport2.java
@@ -0,0 +1,45 @@
+package subreport.simple.fastreport;
+
+public class Subreport2 {
+	
+	private String ref;
+	private String title;
+	private String title1;
+	private String title2;
+	
+	public Subreport2(String ref, String title) {
+		this.ref = ref;
+		this.title = title;
+	}
+	
+	public String getRef() {
+		return ref;
+	}
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle2() {
+		return title2;
+	}
+
+	public void setTitle2(String title2) {
+		this.title2 = title2;
+	}
+
+	public String getTitle1() {
+		return title1;
+	}
+
+	public void setTitle1(String title1) {
+		this.title1 = title1;
+	}
+	
+
+}
